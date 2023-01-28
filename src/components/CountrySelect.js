5f1a8eca496f49a643ee631863716be587498594
Erwan Goudard <erwan.goudard@qonto.com>
2023-01-28 19:06:41 +0100

    Update CountrySelect with countryList

--- src/components/CountrySelect.js
+++ src/components/CountrySelect.js
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
 import Select from "react-select";
 import "../components/CountrySelect.css";
+import { options } from "../countries";
 
 const CountrySelect = ({ countries }) => {
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
-
   return (
     <div>
       <label className="label">Country</label>
-      <Select
-        options={options}
-        onChange={(opt) => console.log(opt.label, opt.value)}
-      />
+      <Select options={options} onChange={(opt) => console.log(opt.value)} />
     </div>
   );
 };
