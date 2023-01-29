f0857147be6e179f659cb18dff34fcebfa924258
Erwan Goudard <erwan.goudard@qonto.com>
2023-01-29 14:01:24 +0100

    Update Country validation

--- src/components/CountrySelect.js
+++ src/components/CountrySelect.js
@@ -1,21 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import "../components/CountrySelect.css";
 import { options } from "../countries";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { updateCountry } from "../redux/user";
 
-const CountrySelect = ({ countries }) => {
+const CountrySelect = ({ triggerValidation }) => {
   const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user);
+  const [countryError, setCountryError] = useState(false);
+
+  useEffect(() => {
+    if (triggerValidation) {
+      validateCountryField();
+    }
+  }, [triggerValidation]);
+
+  const validateCountryField = () => {
+    if (!user.currentUser.country) {
+      setCountryError(true);
+      console.log("whaouuu", user.currentUser.country);
+    }
+  };
+
+  const borderStyle = {
+    control: (base) => ({
+      ...base,
+      border: 0,
+      boxShadow: "none",
+    }),
+  };
+
   return (
     <div>
-      <label className="label">Country</label>
-      <Select
-        options={options}
-        onChange={(opt) => dispatch(updateCountry(opt.value))}
-      />
+      <label data-testid="country-select" className="label">
+        Country
+      </label>
+      <div>
+        <Select
+          className={`country-input ${countryError ? "invalid" : "mb-36"}`}
+          placeholder={<div>Select a country</div>}
+          options={options}
+          id="country-select"
+          onChange={(opt) => {
+            dispatch(updateCountry(opt.value));
+            setCountryError(false);
+          }}
+          styles={borderStyle}
+        >
+          {options.map(({ value, label }) => (
+            <option key={value} value={value} data-testid="CountryOption">
+              {label}
+            </option>
+          ))}
+        </Select>
+        {countryError && (
+          <div className="error-container">
+            <div className="error-message mb-36">Please select a country</div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
